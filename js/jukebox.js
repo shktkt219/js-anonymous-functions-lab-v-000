a1168a718d57554aac8b67e6ce7d96edc8acaa6c
shoko furushita <shktkt219@gmail.com>
2016-09-03 22:11:08 -0400

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -10,31 +10,26 @@ var artists = Object.keys(jukeboxSongs);
 var theArtist = artists[Math.floor(Math.random()) * artists.length];
 var songToPlay = jukeboxSongs[theArtist];
 
-function randomNumber() {
-  return Math.floor(Math.random());
-}
-
 function menu() {
-  $('#menu').on('click', function(e) {
+  $('#menu').on('click', function() {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
   });
 }
 
 function play() {
-  $('#play').on('click', function(e) {
+  $('#play').on('click', function() {
     $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
   });
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
+  $('#pause').on('click', function() {
     $('#songPaused').html(songToPlay + " is paused");
   });
 }
 
 function show() {
-  $('#show').on('click', function(e) {
+  $('#show').on('click', function() {
     var songs = "";
     for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
